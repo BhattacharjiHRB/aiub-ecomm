23353fbe7d5f15d647270f4a37b9cdf08d57cde5
sumshun-nahar-eity <esumshun@gmail.com>
2026-05-11 00:26:32 +0600

    eity, products operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dbService } from './db/db.service';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
 
 @Module({
   controllers: [AppController],
   providers: [AppService, dbService],
   imports: [
     UsersModule,
+    ProductsModule,
 
     ConfigModule.forRoot({
       isGlobal: true,
